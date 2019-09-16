7c2bd549a026ab1a3e97b4087d804e028e954e5c
luiner <luiner@gmail.com>
2019-09-16 18:26:55 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,24 +4,27 @@
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
+    ext._getStatus = function() 
+    {
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.power = function(base, exponent) {
-        return Math.pow(base, exponent);
+    ext.minOf = function(a1, a2) 
+    {
+        if(a1 < a2) return a1;
+        return a2;
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
-            ['r', '%n ^ %n', 'power', 2, 3],
+            ['r', 'Min of %n and %n', 'minOf', 0, 0],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('Math', descriptor, ext);
 })({});
 
 (function(ext) {
